3463f780bdf2ace2d84789e6121d0520382a71a8
Zak Johnson <me@zakj.net>
2017-08-10 19:59:50 -0700

    Reorganize nuxt.config and add analyze script

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  loading: false,
-  head: {
-    htmlAttrs: {lang: 'en'},
-    meta: [
-      {name: 'viewport', content: 'initial-scale=1'},
-    ],
-  },
   css: [
     'normalize.css',
     {src: '~assets/base.styl', lang: 'stylus'},
   ],
+  generate: {
+    minify: {conservativeCollapse: true},
+  },
+  head: {
+    htmlAttrs: {lang: 'en'},
+    meta: [
+      {name: 'charset', content: 'utf-8'},
+      {name: 'viewport', content: 'initial-scale=1'},
+    ],
+  },
+  loading: false,
 };
